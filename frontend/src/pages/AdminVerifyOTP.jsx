eba672fa678152b93d65076f604415fd81113142
aryan6577 <aryanbm6577@gmail.com>
2026-07-20 17:41:18 +0530

    Fix many changes

--- frontend/src/pages/AdminVerifyOTP.jsx
+++ frontend/src/pages/AdminVerifyOTP.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import axios from "../utils/axiosInstance";
+import axios from "../../utils/axiosInstance";
 import { useLocation, useNavigate } from "react-router-dom";
 
 function AdminVerifyOTP() {
