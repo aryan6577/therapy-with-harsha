8ce8173e1bfe04b191431afb90b30189d90af200
aryan6577 <aryanbm6577@gmail.com>
2026-07-21 15:29:45 +0530

    Fix email template quotes

--- backend/services/emailService.js
+++ backend/services/emailService.js
@@ -22,6 +22,10 @@ const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
   port: 465,
   secure: true,
+
+  // Force IPv4 instead of IPv6
+  family: 4,
+
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
